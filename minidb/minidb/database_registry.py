e4850d16c518c2a0d47f9e89947d4cb27720301b
Nastya <yanyshynnastya3@gmail.com>
2025-04-05 21:24:22 +0300

    Added database registry

---
+++ minidb/minidb/database_registry.py
@@ -0,0 +1,22 @@
+"""
+Модуль для глобального реєстру баз даних.
+"""
+
+class DatabaseRegistry:
+    """Глобальний реєстр баз даних."""
+    _databases = {}
+
+    @classmethod
+    def register(cls, name, db_instance):
+        """Реєструє базу даних під заданим ім'ям."""
+        cls._databases[name] = db_instance
+
+    @classmethod
+    def get(cls, name):
+        """Повертає базу даних за ім'ям."""
+        return cls._databases.get(name)
+
+    @classmethod
+    def list(cls):
+        """Повертає список усіх зареєстрованих баз даних."""
+        return list(cls._databases.keys())
